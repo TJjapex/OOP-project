f609cfbb9121aa75424c778f70465670115e0be7
Thomas Verelst <thomas.verelst@hotmail.com>
2015-03-10 19:06:29 +0100

    Added a new way to define the enum (is commented now), I don't know which one is better.

--- src/jumpingalien/model/Orientation.java
+++ src/jumpingalien/model/Orientation.java
@@ -1,6 +1,22 @@
 package jumpingalien.model;
 
+import be.kuleuven.cs.som.annotate.Value;
+
+@Value
 public enum Orientation {
+
+// Nieuwe implementatie:
+//	LEFT(-1), RIGHT(1);
+//	private int sign;
+//
+//	private Orientation(int sign) {
+//		this.sign = sign;
+//	}
+//	
+//	public int getSign(){
+//		return this.sign;
+//	}
+//	
 	LEFT {
 		public int getSign(){
 			return -1;
